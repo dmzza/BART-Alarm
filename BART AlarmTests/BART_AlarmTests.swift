703bce9aa73c6af649be948996b4c8431bd678d2
David Mazza <github@dmzza.com>
2014-12-10 08:20:58 -0800

    Starting commit

---
+++ BART AlarmTests/BART_AlarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  BART_AlarmTests.swift
+//  BART AlarmTests
+//
+//  Created by David Mazza on 12/10/14.
+//  Copyright (c) 2014 Peaking. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BART_AlarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
