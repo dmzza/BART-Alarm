abe57f7c85b9b62e66d15a25c5ef2e22cd6a5727
David Mazza <github@dmzza.com>
2014-12-10 09:34:29 -0800

    Origin VC, Station and Line models

---
+++ BART Alarm/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  BART Alarm
+//
+//  Created by David Mazza on 12/10/14.
+//  Copyright (c) 2014 Peaking. All rights reserved.
+//
+
+import UIKit
+
+class Line: NSObject {
+    var name: NSString!
+    var color: UIColor? = nil
+}
---
+++ BART Alarm/Station.swift
@@ -0,0 +1,14 @@
+//
+//  Station.swift
+//  BART Alarm
+//
+//  Created by David Mazza on 12/10/14.
+//  Copyright (c) 2014 Peaking. All rights reserved.
+//
+
+import UIKit
+
+class Station: NSObject {
+    var name: NSString!
+    var lines: NSArray!
+}
